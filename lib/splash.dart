3c0979542de478ddcfcd8bbde78ddbb43df1ba65
feras allam <feras.allam@gmail.com>
2021-08-11 17:40:26 +0200

    first commit

---
+++ lib/splash.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_loc/main.dart';
+import 'package:provider/provider.dart';
+
+import 'langProvider.dart';
+
+class Splash extends StatefulWidget {
+  LangProvider prlan;
+  Splash({required this.prlan});
+
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  void initState() {
+    super.initState();
+    widget.prlan.getPrefrence();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => MyHomePage(prlan: widget.prlan,))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+        color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
